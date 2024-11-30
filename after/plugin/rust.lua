dfbaac8b7b29833a527db9ab3125409429894925
Le Minh Duong Ha <haleminhduong@gmail.com>
2024-11-30 02:05:26 +0100

    rust-analyzer works

--- after/plugin/rust.lua
+++ after/plugin/rust.lua
@@ -1,12 +1,39 @@
 local rt = require("rust-tools")
+local lspconfig = require('lspconfig')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local on_attach = function(client, bufnr)
+    -- Enable inlay hints
+    vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+end
 
 rt.setup({
-  server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
-  },
+    server = {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    },
+})
+
+lspconfig.rust_analyzer.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = {
+        "rustup", "run", "stable", "rust-analyzer",
+    },
+    settings = {
+        ["rust-analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                allFeatures = true,
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
 })
